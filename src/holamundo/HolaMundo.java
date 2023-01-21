0de5ffa71837238ebcc74366bbf0b3aeeb987820
@sergio.barreras <sergiobg.isc@gmail.com>
2023-01-20 18:25:49 -0700

    Carga inicial

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -3,8 +3,10 @@ package holamundo;
 
 public class HolaMundo {
 
+    //Tercer intento
+    
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, estoy en Github :)");
+        System.out.println("Hola Mundo, estoy en Github :(");
     }
     
 }
