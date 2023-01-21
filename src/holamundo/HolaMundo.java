a85d5b35806e137e1e97c0fec795ab57fd125d23
@sergio.barreras <sergiobg.isc@gmail.com>
2023-01-20 18:27:36 -0700

    Hola desde GitHub

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
     //Tercer intento
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, estoy en Github :(");
+        System.out.println("Hola Mundo, estoy en Github :)");
     }
     
 }
